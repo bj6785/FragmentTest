77105ca63aa876917b61930e6f6f37b479b0956b
kail1 <kail1@verifone.com>
2017-04-11 18:02:07 +0800

    fragment添加menu option功能

--- app/src/main/java/com/verifone/kail1/myapplication/FragmentPage1.java
+++ app/src/main/java/com/verifone/kail1/myapplication/FragmentPage1.java
@@ -2,15 +2,26 @@ package com.verifone.kail1.myapplication;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class FragmentPage1 extends Fragment implements View.OnClickListener {
     private Button bt;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -34,5 +45,28 @@ public class FragmentPage1 extends Fragment implements View.OnClickListener {
                 }
                 break;
         }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.add(Menu.NONE,0, 0, "item1");
+        menu.add(Menu.NONE,1, 1, "item2");
+        menu.add(Menu.NONE,2, 2, "item3");
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 0:
+                Toast.makeText(getActivity(), "item1", Toast.LENGTH_SHORT).show();
+                break;
+            case 1:
+                Toast.makeText(getActivity(), "item2", Toast.LENGTH_SHORT).show();
+                break;
+            case 2:
+                Toast.makeText(getActivity(), "item3", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
